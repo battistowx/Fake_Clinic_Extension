2ede77b196b637c8895251732e9fda0e1b95f03d
Chris Battisto <34325676+battistowx@users.noreply.github.com>
2023-04-26 20:28:49 -0500

    Add logic for adding https:/www, rename vars

--- Fake_Clinic_Extension/js/background.js
+++ Fake_Clinic_Extension/js/background.js
@@ -1,19 +1,41 @@
 // Load csvUrl when the extension is installed
 chrome.runtime.onInstalled.addListener(async () => {
-  const csvUrl = "https://raw.githubusercontent.com/SeanDaBlack/ROE-CPC/main/fake-clinic.csv";
-  const csvData = await fetch(csvUrl).then(response => response.text());
-  chrome.storage.local.set({ csvData });
-  console.log("CSV data downloaded and stored")
+  const csv_url = "https://raw.githubusercontent.com/SeanDaBlack/ROE-CPC/main/fake-clinic.csv";
+  var csv_data = await fetch(csv_url).then(response => response.text());
+
+  csv_data = csv_data.split(",")
+
+  for (let i = 0; i < csv_data.length; i++) {
+    // Replace http with https
+    if (!csv_data[i].startsWith("https")) {
+      csv_data[i] = csv_data[i].replace("http", "https");
+    }
+    // Add "www" to each URL
+    if (!csv_data[i].includes("www")) {
+      csv_data[i] = csv_data[i].replace("https://", "https://www.");
+    }
+  }
+
+  chrome.storage.local.set({ csv_data });
+  console.log("CSV data downloaded and stored");
 });
 
 // Check the URL when the web page is loaded
 chrome.webNavigation.onCompleted.addListener((details) => {
-  chrome.storage.local.get('csvData', (result) => {
-    const csvData = result.csvData;
-    const url = details.url;
-    console.log(url);
-    const csvRows = csvData.split(/\r?\n/);
-    const urls = csvRows.slice(1).map(row => row.split(",")[6].replace('http', 'https'));
+  chrome.storage.local.get(['csv_data'], (result) => {
+    var urls = result.csv_data;
+    var url = details.url;
+
+    // Add "www" to each URL detail
+    if (!url.includes("www")) {
+      url = url.replace("https://", "https://www.");
+    }
+
+    // Uncomment for debugging
+    // console.log(urls);
+    // console.log(url);
+
+    // Check URLs for a match
     if (urls.includes(url)) {
       console.log("URL found");
       chrome.tabs.query({active: true, currentWindow: true}, tabs => {
